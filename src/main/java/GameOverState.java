9b188ece7b9d42aed40a1593e8f584b946cb3d26
Menno Oudshoorn <m.j.j.oudshoorn@student.tudelft.nl>
2015-09-08 10:21:25 +0200

    checkstyle in GameOverState

--- src/main/java/GameOverState.java
+++ src/main/java/GameOverState.java
@@ -6,34 +6,62 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-
+/**
+ * Class that represents the state of the game when the game is over.
+ * @author Menno
+ *
+ */
 public class GameOverState extends BasicGameState {
 
 	private Button playAgainButton;
 	private MainGame mg;
 	
+	private static final int PLAY_AGAIN_BUTTON_X = 300;
+	private static final int PLAY_AGAIN_BUTTON_Y = 275;
+	private static final int PLAY_AGAIN_BUTTON_WIDTH = 200;
+	private static final int PLAY_AGAIN_BUTTON_HEIGHT = 45;
+	
+	private static final int GAME_OVER_X = 100;
+	private static final int GAME_OVER_Y = 100;
+	private static final int POINTS_X = 100;
+	private static final int POINTS_Y = 120;
+	
+	
+	/**
+	 * Constructor.
+	 * @param mg the maingame in which this state will be used.
+	 */
 	public GameOverState(MainGame mg) {
 		this.mg = mg;
 	}
 	
 	/**
-	 * Init method - load resources here
+	 * Init method - load resources here.
+	 * @param container The container this state should be initialized in.
+	 * @param arg1 the StateBasedGame this state is in.
+	 * @throws SlickException idk when
 	 */
 	public void init(GameContainer container, StateBasedGame arg1)
 			throws SlickException {
-		playAgainButton = new Button(300,275,200,45, new Image("resources/play_again_button.png"));
+		playAgainButton = new Button(PLAY_AGAIN_BUTTON_X, PLAY_AGAIN_BUTTON_Y,
+				PLAY_AGAIN_BUTTON_WIDTH, PLAY_AGAIN_BUTTON_HEIGHT,
+				new Image("resources/play_again_button.png"));
 	}
 	
 	/**
-	 * update method - called every frame
+	 * update method - called every frame.
+	 * @param container The container this state is in.
+	 * @param sbg the StateBasedGame this state is in
+	 * @param delta the time in ms since the last frame
+	 * @throws SlickException sometimes.
 	 */
 	public void update(GameContainer container, StateBasedGame sbg, int delta)
 			throws SlickException {
 			Input input = container.getInput();
 			
 			// If the mouse is pressed inside the playAgainButton, enter the gameState
-			if(input.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
-				if(playAgainButton.getRectangle().contains(input.getMouseX(), input.getMouseY())) {
+			if (input.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
+				if (playAgainButton.getRectangle().contains(input.getMouseX(), input.getMouseY())) {
 					mg.resetLifeCount();
 					mg.resetLevelCount();
 					sbg.enterState(1);
@@ -42,19 +70,24 @@ public class GameOverState extends BasicGameState {
 		}
 
 	/**
-	 * Render method - draw things to screen
+	 * Render method - draw things to screen.
+	 * @param container The container this state is in
+	 * @param arg1 the statebasedgame this state is in.
+	 * @param graphics the Graphics used in this gameoverstate.
+	 * @throws SlickException idk when
 	 */
 	public void render(GameContainer container, StateBasedGame arg1, Graphics graphics)
 			throws SlickException {
 		// draw string and button
-		graphics.drawString("Game Over Sucker!", 100, 100);
-		graphics.drawString("Youre score was: " + mg.score, 100, 120);
-		graphics.drawImage(playAgainButton.getImage(), playAgainButton.getX(), playAgainButton.getY());
+		graphics.drawString("Game Over Sucker!", GAME_OVER_X, GAME_OVER_Y);
+		graphics.drawString("Youre score was: " + mg.score, POINTS_X, POINTS_Y);
+		graphics.drawImage(playAgainButton.getImage(), playAgainButton.getX(),
+				playAgainButton.getY());
 	}
 
 	
 	/**
-	 * returns id of the state
+	 * returns id of the state.
 	 */
 	@Override
 	public int getID() {

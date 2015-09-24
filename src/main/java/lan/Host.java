4c02517b0db06a3fafaa3534a9d6c5b587bd27f3
Stefan de Vringer <s.o.devringer@student.tudelft.nl>
2015-09-24 18:41:59 +0200

    Client is now also able to pick up items <feature

--- src/main/java/lan/Host.java
+++ src/main/java/lan/Host.java
@@ -16,8 +16,10 @@ import java.util.concurrent.Callable;
 
 import logic.BouncingCircle;
 import logic.Coin;
+import logic.FloatingScore;
 import logic.Logger;
 import logic.Powerup;
+import logic.Powerup.PowerupType;
 
 /**
  * Host server for LAN multiplayer.
@@ -36,6 +38,8 @@ public class Host implements Callable {
     private MainGame mainGame;
     private GameState gameState;
     private ArrayList<Client> clientList;
+    
+    private static final int THREE = 3;
     
     /**
      * Create a new Host server for LAN multiplayer.
@@ -90,14 +94,83 @@ public class Host implements Callable {
 				if (message2.startsWith("PLAYER")) {
 					System.out.println("wel toch");
 					playerMessage(message2.replaceFirst("PLAYER", ""));
+				} else if (message2.startsWith("POWERUP")) {
+					powerupMessage(message2.replaceFirst("POWERUP", ""));
+				} else if (message2.startsWith("COIN")) {
+					coinMessage(message2.replaceFirst("POWERUP", ""));
 				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
     }
-    
     /**
+     * Javadoc.
+     * @param message .
+     */
+    private void coinMessage(String message) {
+    	String message2 = message.trim();
+    	String[] stringList = message2.split(" ");
+    	if (stringList[THREE].equals("PLEA")) {
+    		ArrayList<Coin> machvise = new ArrayList<Coin>();
+    		for (Coin george : gameState.getDroppedCoins()) {
+    			if (george.getxId() == Float.parseFloat(stringList[0])
+    					&& george.getyId() == Float.parseFloat(stringList[1])) {
+    				machvise.add(george);
+    				this.updateCoinsGrant(george);
+    				gameState.getFloatingScores().add(new FloatingScore(george));
+    			}
+    		}
+    		gameState.getDroppedCoins().removeAll(machvise);
+    	}
+	}
+
+    /**
+     * 
+     * @param coin .
+     */
+    private void updateCoinsGrant(Coin coin) {
+    	sendMessageToClient(coin.toString() + "GRANT ");
+	}
+
+	/**
+     * Javadoc.
+     * @param message .
+     */
+	private void powerupMessage(String message) {
+		String message2 = message.trim();
+    	String[] stringList = message2.split(" ");
+    	if (stringList[THREE].equals("PLEA")) {
+    		ArrayList<Powerup> machvise = new ArrayList<Powerup>();
+    		for (Powerup george : gameState.getDroppedPowerups()) {
+    			if (george.getxId() == Float.parseFloat(stringList[0])
+    					&& george.getyId() == Float.parseFloat(stringList[1])) {
+    				machvise.add(george);
+    				this.updatePowerupsGrant(george);
+    				gameState.getFloatingScores().add(new FloatingScore(george));
+    				if (stringList[2].equals("SHIELD")) {
+    					mainGame.getPlayerList().getPlayers().get(1).addPowerup(PowerupType.SHIELD);
+    				} else if (stringList[2].equals("SPIKY")) {
+    					mainGame.getPlayerList().getPlayers().get(1).addPowerup(PowerupType.SPIKY);
+    				} else if (stringList[2].equals("INSTANT")) {
+    					mainGame.getPlayerList().getPlayers()
+    					.get(1).addPowerup(PowerupType.INSTANT);
+    				}
+    			}
+    		} //end of loop
+    		gameState.getDroppedPowerups().removeAll(machvise);
+    	}
+	}
+
+	/**
+	 * Javadoc.
+	 * @param powerup .
+	 */
+	private void updatePowerupsGrant(Powerup powerup) {
+		sendMessageToClient(powerup.toString() + "GRANT ");
+	}
+
+	/**
      * javadoc.
      * @param message .
      */

be86c832045cd80d96cb2ea40f0efce80ce862e8
Menno Oudshoorn <m.j.j.oudshoorn@student.tudelft.nl>
2015-10-06 16:02:24 +0200

    Set up level 6 <organisation

--- src/main/java/levels/Level6.java
+++ src/main/java/levels/Level6.java
@@ -1,5 +1,48 @@
 package levels;
 
-public class Level6 {
+import java.util.ArrayList;
+
+import gui.MainGame;
+import logic.BouncingCircle;
+import logic.Gate;
+
+/**
+ * This class represents the sixth level.
+ * @author Menno
+ *
+ */
+public class Level6 extends Level {
+	
+	private static final int DEFAULT_BIGBALL_Y = 300;
+	private static final int DEFAULT_YSPEED = -50;
+	private static final float RADIUS_6 = 90;
+	private static final int LEVEL_6_TIME = 120;
+	private static final int LEVEL_6_BALL_X = 900;
+
+	/**
+	 * Construct a new Level6.
+	 * @param mainGame the MainGame that uses this level
+	 */
+	public Level6(MainGame mainGame) {
+		super(mainGame);
+	}
+	
+	@Override
+	public void constructLevel() {
+		// The circle list
+		ArrayList<BouncingCircle> circles = new ArrayList<BouncingCircle>();
+		circles.add(new BouncingCircle(LEVEL_6_BALL_X, DEFAULT_BIGBALL_Y, RADIUS_6,
+				-this.getMaingame().getStartingSpeed(), DEFAULT_YSPEED,
+				this.getMaingame().getGravity(), 0));
+		
+		// The gate list
+		ArrayList<Gate> gates = new ArrayList<Gate>();
+		
+		// Set the correct attributes
+		this.setCircles(circles);
+		this.setGates(gates);
+		this.setTime(LEVEL_6_TIME);
+
+	}
 
 }
